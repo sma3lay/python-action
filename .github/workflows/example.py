9d18aa397f9b9ca2096d080b6f5e65c867272be2
sma3lay <93422454+sma3lay@users.noreply.github.com>
2022-11-13 14:10:54 +0630

    change test

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,9 +1,20 @@
- 
+from selenium import webdriver
+import time, random
 
-def main():
-  print("Hello from GitHub Actions!")
-   
-  
-
-if __name__ == '__main__':
-  main()
+links = ["https://shwepin.xyz/?p=221659",
+         "https://shwepin.xyz"]
+count =0;
+while True:
+    for link in links:
+        driver = webdriver.Chrome()
+        driver.get(link)
+        print("Browser is reading...")
+        height=driver.execute_script("return document.body.scrollHeight")
+        Y=height/5
+        for i in range(0,5):
+            driver.execute_script("window.scrollTo(0,{})".format(Y))
+            time.sleep(random.randint(5,15))
+            Y+=Y
+        count+=1
+        print(count)
+        driver.quit()
